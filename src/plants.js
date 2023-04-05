a5c3f94a85a19f1099f34821bebf99bf8a60d2f6
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-04-05 17:12:31 -0400

    fixed bug and updated plant

--- src/plants.js
+++ src/plants.js
@@ -26,12 +26,13 @@ export async function deletePlant(req, res) {
 
 // CRUD: UPDATE - update/patch
 export async function updatePlant(req, res) {
-  const docId = { "_id": new ObjectId(req.params.docId)};
-  const updatePlant = req.body;
 
-  await coll.findOneAndUpdate(
-    { "_id": docId },
-    {$set: {updatePlant} }
-  );
-  res.status(201).send( {message: "plant has been updated"})
+  const docId = { "_id": new ObjectId(req.params.docId)};
+  const updatePlant = { $set: req.body };
+  const returnOption = { returnNewDocument: true}
+
+  const query = await coll.findOneAndUpdate(docId, updatePlant, returnOption);
+
+  res.status(201).send( {message: "plant has been updated"}) 
+  console.table(query.value);
 }
